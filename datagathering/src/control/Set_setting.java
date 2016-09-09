e67b18f376396ebaa19deb6be0d54b551046147a
peymanvalikhanli <peymanvalikhanli2012@gmail.com>
2016-09-09 11:40:42 +0430

    file reading

--- datagathering/src/control/Set_setting.java
+++ datagathering/src/control/Set_setting.java
@@ -13,9 +13,6 @@ public class Set_setting extends File_IO  {
 
 	
 	public int read_type_file(Context cotx) {
-		// reads setup parameters from hs_setup.csv file in Vars.Setup_path into GlobalAccess.Vars class
-		// returns 0 if no error, otherwise error code reference
-			//test commit 
 			String []  csv_row = new String[100];
 			
 			Access access = new Access(cotx);
@@ -40,13 +37,101 @@ public class Set_setting extends File_IO  {
 			} catch (IOException e) {
 				String error_log = "SetupParams_IO; read_from_file; e.toString():" + e.toString() + "\n e.getMessage():" + e.getMessage() + "\n e.getLocalizedMessage():" + e.getLocalizedMessage();
 				Log.e("error", error_log);
-				//SensorInterfaceEngine.updateLogFile(error_log);
 				e.printStackTrace();
 				return 12; // unable to open file
 			}
 		    
 		}	
 		        	
+	public int read_company_file(Context cotx) {
+		String []  csv_row = new String[100];
+		
+		Access access = new Access(cotx);
+		access.del_Company(); 
+	    try {
+			csv_row = this.readFromFile(Vars.Company_path);
+			int i = 0;
+	        while ( csv_row[i] != null ) {
+	        	String[] column = csv_row[i].split(",");
+	        	access.insert_company(column[i]);
+	          	i++;
+	        }
+	        
+	        return 1 ; 
+		} catch (IOException e) {
+			String error_log = "SetupParams_IO; read_from_file; e.toString():" + e.toString() + "\n e.getMessage():" + e.getMessage() + "\n e.getLocalizedMessage():" + e.getLocalizedMessage();
+			Log.e("error", error_log);
+			e.printStackTrace();
+			return 12; // unable to open file
+		}
+	    
+	}	
+	
+	public int read_application_file(Context cotx) {
+		String []  csv_row = new String[100];
+		
+		Access access = new Access(cotx);
+		access.del_application();
+		int id_counter = 1 ; 
+	    try {
+			csv_row = this.readFromFile(Vars.Application_path);
+			int i = 0;
+	        while ( csv_row[i] != null ) {
+	        	String[] column = csv_row[i].split(",");
+	    		int parent_id = id_counter ;
+	        	access.insert_Application(column[0], "0" , String.valueOf(id_counter));
+	        	id_counter++; 
+	        	for(int j = 1 ; j< column.length ; j++ ){
+	        		access.insert_Application(column[j], String.valueOf(parent_id) , String.valueOf(id_counter));
+	        		id_counter++; 
+	        	}
+	        	i++;
+	        }
+	        
+	        return 1 ; 
+		} catch (IOException e) {
+			String error_log = "SetupParams_IO; read_from_file; e.toString():" + e.toString() + "\n e.getMessage():" + e.getMessage() + "\n e.getLocalizedMessage():" + e.getLocalizedMessage();
+			Log.e("error", error_log);
+			//SensorInterfaceEngine.updateLogFile(error_log);
+			e.printStackTrace();
+			return 12; // unable to open file
+		}
+	    
+	}	
+	
+	public int read_ancill_file(Context cotx) {
+		String []  csv_row = new String[100];
+		
+		Access access = new Access(cotx);
+		access.del_type();
+		int id_counter = 1 ; 
+	    try {
+			csv_row = this.readFromFile(Vars.Ancill_path);
+			int i = 0;
+	        while ( csv_row[i] != null ) {
+	        	String[] column = csv_row[i].split(",");
+	    		int parent_id = id_counter ;
+	        	access.insert_type(column[0], "0" , String.valueOf(id_counter));
+	        	id_counter++; 
+	        	for(int j = 1 ; j< column.length ; j++ ){
+	        		access.insert_type(column[j], String.valueOf(parent_id) , String.valueOf(id_counter));
+	        		id_counter++; 
+	        	}
+	        	i++;
+	        }
+	        
+	        return 1 ; 
+		} catch (IOException e) {
+			String error_log = "SetupParams_IO; read_from_file; e.toString():" + e.toString() + "\n e.getMessage():" + e.getMessage() + "\n e.getLocalizedMessage():" + e.getLocalizedMessage();
+			Log.e("error", error_log);
+			//SensorInterfaceEngine.updateLogFile(error_log);
+			e.printStackTrace();
+			return 12; // unable to open file
+		}
+	    
+	}	
+
+
 
 	
 }
